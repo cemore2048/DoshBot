6b803aed6f97500948ac594c85bd082e128a87f7
Rafa <rmoreno.cesar@gmail.com>
2020-05-17 17:36:59 -0500

    Install nodemon and accept challenge token from Slack

--- src/server.ts
+++ src/server.ts
@@ -3,9 +3,15 @@ import express = require("express")
 
 const PORT = process.env.PORT || 5000
 const app: express.Application = express()
+app.use(express.json())
+
+app.post('/challenge', function(req, res) {
+    const challenge = req.body["challenge"]
+    res.send(challenge)
+})
 
 app.get('/', function(req, res) {
-    res.send("Hello World")
+    res.send("Good Morning Sunshine, the earth says hello!")
 })
 
 app.listen(PORT, function() {
